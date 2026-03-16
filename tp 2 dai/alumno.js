4a4fe8c69ee699dc8ea6a1e440f19f9b00a37c73
CATALINA COHEN <49092736@ort.edu.ar>
2026-03-16 08:07:24 -0300

    ej 3

---
+++ tp 2 dai/alumno.js
@@ -0,0 +1,10 @@
+let username;
+let dni; 
+
+function usuario(username, dni) {
+    username = username;
+    dni = dni;
+    return { username, dni }
+}
+
+export {usuario};
--- tp 2 dai/index.js
+++ tp 2 dai/index.js
@@ -5,3 +5,8 @@ import matematica from "./matematica.js";
 console.log(matematica.sumar(1,3));
 console.log(matematica.restar(1,6));
 console.log(matematica.Pi());
+
+import {usuario} from "./alumno.js";
+
+console.log(usuario("cata", 12345678));
+console.log(usuario("chloe", 45903499));
